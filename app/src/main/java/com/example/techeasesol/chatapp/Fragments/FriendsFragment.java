731b8c6e3032105026917d5bd4fcfb2d21e25ae4
Developer Abdullah <35663915+softwrengr@users.noreply.github.com>
2018-03-22 10:22:52 +0500

    second

--- app/src/main/java/com/example/techeasesol/chatapp/Fragments/FriendsFragment.java
+++ app/src/main/java/com/example/techeasesol/chatapp/Fragments/FriendsFragment.java
@@ -8,6 +8,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.techeasesol.chatapp.R;
 
@@ -19,6 +20,7 @@ public class FriendsFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_friends, container, false);
         Button btn  = new Button(getActivity());
+        EditText editText = new EditText(getActivity());
         return view;
     }
 }
