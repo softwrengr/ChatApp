6e72ae11c73565ae912cb1f99eab6c7c5ea8a833
Developer Abdullah <35663915+softwrengr@users.noreply.github.com>
2018-03-22 10:19:38 +0500

    Friebds Fragment added

--- app/src/main/java/com/example/techeasesol/chatapp/Fragments/FriendsFragment.java
+++ app/src/main/java/com/example/techeasesol/chatapp/Fragments/FriendsFragment.java
@@ -7,6 +7,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.techeasesol.chatapp.R;
 
@@ -17,6 +18,7 @@ public class FriendsFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_friends, container, false);
+        Button btn  = new Button(getActivity());
         return view;
     }
 }
